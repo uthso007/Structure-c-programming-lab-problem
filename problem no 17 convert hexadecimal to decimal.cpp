cdb9594e005c01225eab6799a4cf5553073d66e0
uthso007 <102689737+uthso007@users.noreply.github.com>
2022-04-04 19:44:11 +0600

    Add files via upload

---
+++ problem no 17 convert hexadecimal to decimal.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ long int num;
+ printf("Enter a Hexadecimal integer number: ");
+ scanf("%x",&num);
+ printf("\n\nThe equivalent decimal number is: %d",num);
+
+ getch();
+ return 0;
+}
+
