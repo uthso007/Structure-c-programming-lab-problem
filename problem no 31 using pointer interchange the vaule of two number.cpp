30906b7d472112e8bf7c1705945c2e379c4b6cef
uthso007 <102689737+uthso007@users.noreply.github.com>
2022-04-09 21:32:51 +0600

    Update problem no 31 using pointer interchange the vaule of two number.cpp

--- problem no 31 using pointer interchange the vaule of two number.cpp
+++ problem no 31 using pointer interchange the vaule of two number.cpp
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void exchange(float *a, float *b);
+
+int main()
+{
+ float x,y;
+ printf("Enter the vaule of the two number: ");
+ scanf("%f %f",&x,&y);
+ printf("\n Before exchange x= %f, y=%f", x,y);
+ exchange(&x,&y);
+ printf("\n\n After exchange  x= %f, y=%f", x,y);
+ getch();
+ return 0;
+}
+
+void exchange(float *a, float *b)
+ {
+   float t;
+   t=*a;
+   *a=*b;
+   *b=t;
+ }
 
